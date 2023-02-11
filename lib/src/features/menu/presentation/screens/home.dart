92108da6052ef3b8f34078e249b2469f72141e64
Boburbek <boburbekking@gmail.com>
2023-02-11 18:34:31 +0500

    testing

---
+++ lib/src/features/menu/presentation/screens/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:yandex_mapkit/yandex_mapkit.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: YandexMap(
+        nightModeEnabled: true,
+      ),
+    );
+  }
+}
